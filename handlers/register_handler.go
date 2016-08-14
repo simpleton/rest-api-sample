3dd92bc788a615909a72f3fb32c73f4811e2f8bc
simsun <sunsj1231@gmail.com>
2016-08-14 09:30:52 +0800

    test(register): return the same body for test

--- handlers/register_handler.go
+++ handlers/register_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"github.com/labstack/echo"
 	"net/http"
+	"github.com/simpleton/tinker-api/models"
 ***REMOVED***
 
 type RegisterHandler struct {
@@ -10,5 +11,9 @@ type RegisterHandler struct {
 }
 
 func(this *RegisterHandler***REMOVED*** Register(c echo.Context***REMOVED*** error {
-	return c.String(http.StatusOK, "Hello, World! Register!"***REMOVED***
+	register := new(models.RegisterInfo***REMOVED***
+	if err := c.Bind(register***REMOVED***; err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, register***REMOVED***
 }
